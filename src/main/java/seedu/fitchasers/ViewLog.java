374de40c5682da5c095ceeb3c6c13fded10afd37
Zhong Baode <z5b3d3@gmail.com>
2025-10-21 17:38:23 +0800

    Removed unused import Optional

--- src/main/java/seedu/fitchasers/ViewLog.java
+++ src/main/java/seedu/fitchasers/ViewLog.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 
 public class ViewLog {
     public  static final int MINIMUM_PAGE_SIZE = 1;
