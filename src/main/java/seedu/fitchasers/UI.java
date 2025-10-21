4443b7ceea0238414853de843ae7e9fa8885372d
Zhong Baode <z5b3d3@gmail.com>
2025-10-21 17:36:21 +0800

    Fix Indentation

--- src/main/java/seedu/fitchasers/UI.java
+++ src/main/java/seedu/fitchasers/UI.java
@@ -219,10 +219,10 @@ public class UI {
             return "th";
         }
         return switch (day % 10) {
-            case 1 -> "st";
-            case 2 -> "nd";
-            case 3 -> "rd";
-            default -> "th";
+        case 1 -> "st";
+        case 2 -> "nd";
+        case 3 -> "rd";
+        default -> "th";
         };
     }
 }
