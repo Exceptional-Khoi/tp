7da7e0b19e949853ede0b682c5d21cc1b5911ae4
Zhong Baode <z5b3d3@gmail.com>
2025-11-03 12:48:27 +0800

    Weird CL test

--- src/test/java/seedu/fitchasers/DeleteWorkoutTest.java
+++ src/test/java/seedu/fitchasers/DeleteWorkoutTest.java
@@ -45,7 +45,7 @@ class DeleteWorkoutTest {
         }
 
         @Override
-        public boolean confirmationMessage() {
+        public Boolean confirmationMessage() {
             return nextConfirm;
         }
     }
